9e1dee447fbafc4f15260a5ddc2aeed12483a963
Valerio Mascagna <valerio.mascagna@unibs.it>
2023-11-30 11:38:11 +0100

    Minor mod. (more comments in the script)

--- create_JINST_author_list.py
+++ create_JINST_author_list.py
@@ -6,14 +6,14 @@ import string
 alphabet_letters = list(string.ascii_lowercase)
 alphabet_letters.extend(['a'+l for l in alphabet_letters])
 
+# Open the text file copied from ENUBET internal wiki and import its contend in "lines"
 with open('author_list.txt') as fp:
     lines = fp.readlines()
 
 
+# Create the "author" list of dictionaries for the authors
 author_list = lines[0].replace("),", "),;").split(";")
-
 authors = []
-
 for a in author_list:
     name = ' '.join(a.replace('.','.$').split('$')[:-1]).strip()
     surname = a.replace('.','.$').split('$')[-1].split("(")[0].strip()
@@ -23,13 +23,13 @@ for a in author_list:
                     'surname': surname,
                     'affiliations'  : affiliations})
 
-
+# Create an affiliation dict (starting from the affiliation list from the text file)
 affiliation_list = [l.rstrip("\n") for l in lines[2:]]
 affiliation_dict = {l.split(" ")[0].lstrip("(").rstrip(")"): " ".join(l.split(" ")[1:]) for l in affiliation_list}
 
 
-# visit the aff list in alphabetical order of the names
-# when a new aff keys is encountered, assign to a letter and remove from the keys
+# Visit the affiliation dict in alphabetical order (names as listed in the wiki)
+# When a new aff keys is encountered, assign to a letter and remove it from the keys
 all_aff_keys = list(affiliation_dict.keys())
 letter_index = 0
 num_to_letter_aff_map = {}
@@ -41,14 +41,15 @@ for a in authors:
             all_aff_keys.remove(aff)
             
 
-# OUTPUT TEXT (to the stdout)
+#############################
+# print out the results (to the stdout)
+#############################
 
 # Author list
 for a in authors:
     aff_keys = [num_to_letter_aff_map[num] for num in a['affiliations']]
     print(f"\\author[{','.join(aff_keys)}]{{{a['name']} {a['surname']}}}")
-
-print('\n')
+print('\n') #  black line
 # Affiliation list
 for num in num_to_letter_aff_map:
     print(f'\\affiliation[{num_to_letter_aff_map[num]}]{{{affiliation_dict[num]}}}')
